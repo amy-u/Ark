7ac3e0158d3b3ec776f6b2e038ddbdbaba07d836
Amy Ung <amyung.lm@gmail.com>
2021-03-20 20:35:42 -0700

    Created MedicalVet, draft, no styling

---
+++ Ark/pages/MedicalVet.js
@@ -0,0 +1,21 @@
+import { Button, TextField } from "@material-ui/core";
+
+export default function MedicalVet() {
+  return (
+    <div>
+      <h1>Your Medical and Vet Notes</h1>
+
+      {/* Get home button working */}
+      <Button href="/">Home</Button>
+
+      <h2>Notes:</h2>
+      {/* Button to go back to home directory */}
+      <TextField variant="outlined" />
+      <Button>Save</Button>
+
+      {/* Text box */}
+
+      {/* Picture album */}
+    </div>
+  );
+}
